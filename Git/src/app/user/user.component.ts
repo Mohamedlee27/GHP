2d047b4a1aae439e850105a6e90bad03399d2122
Mohamedlee27 <leonk8070@gmail.com>
2022-04-28 23:59:46 +0300

    add main function of calling information from github

--- Git/src/app/user/user.component.ts
+++ Git/src/app/user/user.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
 
-  constructor() { }
+export class UserComponent implements OnInit {
+userLabel = 'Mohamedlee27'
+user!:any;
+repo!:any;
+  constructor(private ServiceService:ServiceService) { }
+  callInfo(newPerson:any):void{
+    this.userLabel = newPerson
+
+    this.ServiceService.fetchRepo(this.userLabel).subscribe((repo)=>{
+      this.repo = repo
+      console.log (repo);
+    })
+
+    this.ServiceService.serveUser(this.userLabel).then((user)=>(this.user=user))
+
+  } 
 
   ngOnInit(): void {
   }
