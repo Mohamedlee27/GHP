7b517f7b7e9942a16371e13878a56e63dd5b9957
Mohamedlee27 <leonk8070@gmail.com>
2022-04-29 02:50:54 +0300

    created search form component

--- Git/src/app/app.module.ts
+++ Git/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
